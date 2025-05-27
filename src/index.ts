a9ded501c76068bfb65f3892f42e01778d38a757
Kevin Jacob <kevin.jacob@kevin.jacob-C02FH4CBMD6N>
2025-05-27 16:33:14 +0530

    working with teams

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,10 @@ import {
     ConfigurationBotFrameworkAuthentication,
     ConfigurationBotFrameworkAuthenticationOptions
 } from 'botbuilder';
-
 // This bot's main dialog.
-import { EchoBot } from './bot';
-
+import { EchoBot } from './bot.js';
 import { config } from 'dotenv';
+
 const ENV_FILE = path.join(__dirname, '..', '.env');
 config({ path: ENV_FILE });
 
@@ -23,14 +22,15 @@ config({ path: ENV_FILE });
 const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log(`\n${server.name} listening to ${server.url}`);
     console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
 });
 
+
 const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(process.env as ConfigurationBotFrameworkAuthenticationOptions);
 
-
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
 const adapter = new CloudAdapter(botFrameworkAuthentication);
 
 // Catch-all for errors.
@@ -38,12 +38,12 @@ const onTurnErrorHandler = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
     // NOTE: In production environment, you should consider logging this to Azure
     //       application insights.
-    console.error(`\n [onTurnError] unhandled error: ${ error }`);
+    console.error(`\n [onTurnError] unhandled error: ${error}`);
 
     // Send a trace activity, which will be displayed in Bot Framework Emulator
     await context.sendTraceActivity(
         'OnTurnError Trace',
-        `${ error }`,
+        `${error}`,
         'https://www.botframework.com/schemas/error',
         'TurnError'
     );
@@ -61,8 +61,21 @@ const myBot = new EchoBot();
 
 // Listen for incoming requests.
 server.post('/api/messages', (req, res, next) => {
+    console.log(`\nReceived request: ${req}`);
     // Route received a request to adapter for processing
     adapter.process(req, res, async (context) => await myBot.run(context));
+});
+
+server.get('/', (req, res, next) => {
+    console.log(`\nReceived request: ${req}`);
+    res.send({ message: 'Hello from your Restify API!' });
+  return next();
+})
+
+// Handle undefined routes
+server.get('*', (req, res,next) => {
+    res.json({ error: 'Route not found' });
+    return next();
 });
 
 // Listen for Upgrade requests for Streaming.
